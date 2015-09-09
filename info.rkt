87ebbbc124108e311455c2db4096342a9acd857c
Racket Package Dependency Fixer <jackhfirth+racket-dep-fixer@gmail.com>
2015-09-09 19:08:35 +0000

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,4 +1,6 @@
 #lang setup/infotab
 (define deps '("typed-racket-lib" "base"))
-(define build-deps '("scribble-lib"))
+(define build-deps '("racket-doc"
+                     "typed-racket-doc"
+                     "scribble-lib"))
 (define collection 'multi)
